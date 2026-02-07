1f9b6f34f727d59652516fcd4f166f6f5a483881
samani21 <eksloba21@gmail.com>
2026-02-07 14:02:49 +0800

    Perbaiki warna di products

--- src/Components/Store/Header.tsx
+++ src/Components/Store/Header.tsx
@@ -30,7 +30,7 @@ const HeaderStore = ({ categories }: Props) => {
         <header className="sticky top-0 z-40 bg-slate-100 backdrop-blur-lg border-b border-neutral-100">
             <div className="max-w-screen-md mx-auto px-4 py-3 flex justify-between items-center">
                 <div className="flex flex-col cursor-pointer" onClick={() => route.push('/')}>
-                    <h1 className="text-lg font-extrabold tracking-tight text-emerald-900 leading-none">
+                    <h1 className="text-lg font-extrabold tracking-tight text-blue-900 leading-none">
                         PURE<span className="font-light text-neutral-400">EATS</span>
                     </h1>
                     <span className="text-[10px] text-neutral-400 uppercase tracking-widest font-medium">Coffee & Eatery</span>
@@ -50,7 +50,7 @@ const HeaderStore = ({ categories }: Props) => {
                         key={cat.id}
                         onClick={() => scrollToCategory(cat.name)}
                         className={`flex items-center space-x-1.5 whitespace-nowrap px-4 py-2 rounded-full text-xs font-semibold transition-all duration-300 ${activeTab === cat.name
-                            ? 'bg-emerald-900 text-white shadow-md'
+                            ? 'bg-blue-900 text-white shadow-md'
                             : 'bg-neutral-100 text-neutral-500'
                             }`}
                     >
